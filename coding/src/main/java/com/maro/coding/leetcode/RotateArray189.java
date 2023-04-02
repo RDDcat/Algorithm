1d59fd9cc13bab516408abb3cd93b11cf9b972b7
RDDcat <yukmaro@gmail.com>
2023-04-03 00:13:53 +0900

    [UPDATE] RotateArray189 풀이

--- coding/src/main/java/com/maro/coding/leetcode/RotateArray189.java
+++ coding/src/main/java/com/maro/coding/leetcode/RotateArray189.java
@@ -1,7 +1,18 @@
 package com.maro.coding.leetcode;
 
+import java.util.Arrays;
+
 public class RotateArray189 {
     public int[] rotate(int[] nums, int k) {
+        int[] result = nums.clone();
+        int index =0;
+        for (int n:result) {
+            while(index+k >= nums.length){
+                index -= nums.length;
+            }
+            nums[index+k] = n;
+            index++;
+        }
 
         return nums;
     }
