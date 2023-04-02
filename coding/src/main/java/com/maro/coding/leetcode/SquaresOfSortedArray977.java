fd246dc25a5fd48cb7660a1186cfbcd2fad0d202
RDDcat <yukmaro@gmail.com>
2023-04-02 23:05:00 +0900

    [UPDATE] squares of a sorted array 977 Accepted version

--- coding/src/main/java/com/maro/coding/leetcode/SquaresOfSortedArray977.java
+++ coding/src/main/java/com/maro/coding/leetcode/SquaresOfSortedArray977.java
@@ -1,9 +1,34 @@
 package com.maro.coding.leetcode;
 
+import java.util.Arrays;
+
 public class SquaresOfSortedArray977 {
     public int[] sortedSquares(int[] nums) {
+        int[] squares = new int[nums.length];
+        int index = 0;
+        // 투포인터
+        int[] result = new int[nums.length];
+        int start = 0;
+        int end = nums.length-1;
+
+        for (int n:nums) {
+            squares[index++] = (int) Math.pow(n,2);
+        }
+//        System.out.println(Arrays.toString(squares));
 
 
-        return null;
+        while(index > 0){
+            if(squares[start] > squares[end]){
+                result[--index] = squares[start];
+                start++;
+            }
+            else {
+                result[--index] = squares[end];
+                end--;
+            }
+        }
+//        System.out.println(Arrays.toString(result));
+
+        return result;
     }
 }
