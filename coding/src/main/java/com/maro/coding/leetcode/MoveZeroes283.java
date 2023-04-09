1d6d2f6d3214c5b9b01fd608c43ce4d86416c476
yukmaro <2018158018@tukorea.ac.kr>
2023-04-10 06:52:16 +0900

    [UPDATE] 283. Move Zeroes 풀이

--- coding/src/main/java/com/maro/coding/leetcode/MoveZeroes283.java
+++ coding/src/main/java/com/maro/coding/leetcode/MoveZeroes283.java
@@ -3,6 +3,16 @@ package com.maro.coding.leetcode;
 public class MoveZeroes283 {
     // https://leetcode.com/problems/move-zeroes/?envType=study-plan&id=algorithm-i
     public int[] moveZeroes(int[] nums) {
+        int index=0;
+        for (int num:nums) {
+            if(num != 0){
+                nums[index++] = num;
+            }
+        }
+        for (int i = index; i < nums.length; i++){
+            nums[i] = 0;
+        }
+
 
         return nums;
     }
